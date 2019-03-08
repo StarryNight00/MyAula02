8c0ff3b8c1d18e9bbf7fb373358660600a3a76d0
StarryNight00 <cisa.matias@gmail.com>
2019-03-08 12:05:54 +0000

    adição de prog Cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nCilindro");
+            // Declarar as variáveis
+            string altStr;
+            string radStr;
+
+
+            //Input
+            Console.WriteLine("Insira os valores para:\n");
+            //Pedir altura e converter para Float/Double
+            Console.WriteLine("Altura: ");
+            altStr = Console.ReadLine();
+            //Pedir raio e converter para Float/Double
+            Console.WriteLine("Raio: ");
+            radStr = Console.ReadLine();
+
+            //Operações
+            //Volume = 
+            //Área Superfícial
+
+            //Print Resultados
+        }
+    }
+}
--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace VariasStrings
 {
@@ -6,7 +7,30 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Exercício 1");
+            string s1 = "My name is \t, what is yours,\n my friend?";
+            string s2 = @"My name is \t, what is yours,\n my friend?";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            Console.WriteLine("\n\nExercício 2");
+
+            Console.WriteLine("\n\nExercício 3");
+            double xx = 0.12345;
+            int ii = 18;
+
+            string s9 = $"{xx:f2}";
+            string s10 = $"{xx:p1}";
+            string s11 = $"{ii:x}";
+            string s12 = $"{ii:c}";
+
+            Console.WriteLine(s9);
+            Console.WriteLine(s10);
+            Console.WriteLine(s11);
+            Console.WriteLine(s12);
         }
     }
 }

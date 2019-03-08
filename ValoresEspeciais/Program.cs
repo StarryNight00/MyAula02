551acb75c22482081271bc30244f4a7830f998b6
StarryNight00 <cisa.matias@gmail.com>
2019-03-08 13:00:32 +0000

    Valores Especiais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int
+            Console.WriteLine("Max de int é " + int.MaxValue + " e o min é" + int.MinValue);
+
+
+            //double
+            double x1 = double.PositiveInfinity;
+            double x2 = double.NegativeInfinity;
+            Console.WriteLine("Max de double é "+ double.MaxValue + " e o min é" + double.MinValue);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+    }
+}

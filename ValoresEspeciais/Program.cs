6a5b0d1c6677e02e693f178bdb40bb2af46ebf5e
StarryNight00 <cisa.matias@gmail.com>
2019-03-20 09:54:09 +0000

    nada adicionado, just testin

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -13,9 +13,16 @@ namespace ValoresEspeciais
             //double
             double x1 = double.PositiveInfinity;
             double x2 = double.NegativeInfinity;
-            Console.WriteLine("Max de double é "+ double.MaxValue + " e o min é" + double.MinValue);
+            Console.WriteLine("\nMax de double é "+ double.MaxValue + " e o min é" + double.MinValue);
             Console.WriteLine(x1);
             Console.WriteLine(x2);
+
+            //float
+            float f1 = float.PositiveInfinity;
+            float f2 = float.NegativeInfinity;
+            Console.WriteLine("\nMax de float é " + float.MaxValue + " e o min é" + float.MinValue);
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
         }
     }
 }

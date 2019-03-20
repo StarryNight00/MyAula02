92e410f83796f831be2d730cdbf483a502830bc1
StarryNight00 <cisa.matias@gmail.com>
2019-03-20 10:16:32 +0000

    + exercicio 2 em VariasStrings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -10,13 +10,22 @@ namespace VariasStrings
             Console.OutputEncoding = Encoding.UTF8;
 
             Console.WriteLine("Exercício 1");
-            string s1 = "My name is \t, what is yours,\n my friend?";
-            string s2 = @"My name is \t, what is yours,\n my friend?";
+            string s1 = "My name is \t, what is yours,\n my friend? \u263A";
+            string s2 = @"My name is \t, what is yours,\n my friend? \u263A";
 
             Console.WriteLine(s1);
             Console.WriteLine(s2);
 
             Console.WriteLine("\n\nExercício 2");
+            int x = 2;
+            string y = "papagaio";
+            string w = "verde";
+            string z = "azul";
+
+            string s3 = String.Format("Isto é x = {0} e isto é y = {1}", x, y);
+            Console.WriteLine(s3);
+            Console.WriteLine("O céu é {0}", w);
+            Console.WriteLine($"Mas dizem que pode ser {w + z}");
 
             Console.WriteLine("\n\nExercício 3");
             double xx = 0.12345;
